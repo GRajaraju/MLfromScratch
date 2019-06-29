eeee3410da76c238c3d2624ebee5676ae818394d
Raja Raju <jungleecode@users.noreply.github.com>
2019-06-29 17:40:46 +0530

    removed unused code

--- logistic_regression_numpy.py
+++ logistic_regression_numpy.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
 # fake data
 x = np.random.randn(1000, 1)
@@ -8,9 +7,6 @@ y = np.array([1]*500 + [0]*500).reshape(1000, 1)
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
-
-x_train, x_test, y_train, y_test = train_test_split(x, y,
-                       test_size=.20, random_state=42)
 
 w = np.random.randn(1, 1)
 
